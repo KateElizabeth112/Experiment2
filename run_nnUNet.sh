c9589e71ef1a0fc435f90941d61284d3f419c7a6
Kate Cevora <katecevora@kates-mbp.home>
2023-07-19 10:12:08 +0100

    Updated to dataset 3

--- run_nnUNet.sh
+++ run_nnUNet.sh
@@ -12,7 +12,7 @@ source venv/bin/activate
 # Set environment variables
 #ROOT_DIR='/Users/katecevora/Documents/PhD/data/TotalSegmentator_nnUNet/'
 ROOT_DIR='/vol/biomedic3/kc2322/data/TotalSegmentator_nnUNetv1/'
-TASK='Task601_Mixed'
+TASK='Task603_Men'
 
 export nnUNet_raw_data_base=$ROOT_DIR"nnUNet_raw_data_base"
 export nnUNet_preprocessed=$ROOT_DIR"nnUNet_preprocessed"
@@ -24,10 +24,10 @@ echo $RESULTS_FOLDER
 
 # Run script to generate dataset json
 echo "Generating dataset..."
-#python3 generateDatasetJson.py -r $ROOT_DIR -n $DS -tc Task301
+python3 generateDatasetJson.py
 
 echo "Planning and precprocessing..."
-#nnUNet_plan_and_preprocess -t 601 --verify_dataset_integrity
+nnUNet_plan_and_preprocess -t 603 --verify_dataset_integrity
 
 nnUNet_train 2d nnUNetTrainerV2 $TASK 0
 
