1261f67180270404b9a66e42a11e0c1b1a46b174
Kate Cevora <kate.wilkinson@illumr.com>
2023-08-16 14:18:40 +0100

    Updated slurm requirements

--- run_nnUNet.sh
+++ run_nnUNet.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Example of running python script in a batch mode
-#SBATCH -c 4 # Number of CPU Cores
+#SBATCH -c 12 # Number of CPU Cores
 #SBATCH -p gpushigh # Partition (queue)
 #SBATCH --gres gpu:1 # gpu:n, where n = number of GPUs
-#SBATCH --mem 32G # memory pool for all cores
+#SBATCH --mem 64G # memory pool for all cores
 #SBATCH --nodelist monal03 # SLURM node
 #SBATCH --output=slurm.%N.%j.log # Standard output and error log
 # Launch virtual environment
@@ -12,7 +12,7 @@ source /vol/biomedic3/kc2322/code/AMOS_3D/venv/bin/activate
 # Set environment variables
 #ROOT_DIR='/Users/katecevora/Documents/PhD/data/TotalSegmentator_nnUNet/'
 ROOT_DIR='/vol/biomedic3/kc2322/data/TotalSegmentator_nnUNet/'
-TASK='Dataset304_Mini'
+TASK='Dataset301_Set1'
 
 export nnUNet_raw=$ROOT_DIR"nnUNet_raw"
 export nnUNet_preprocessed=$ROOT_DIR"nnUNet_preprocessed"
@@ -23,7 +23,7 @@ echo $nnUNet_preprocessed
 echo $nnUNet_results
 
 # Plan and preprocess data
-nnUNetv2_plan_and_preprocess -d 304 --verify_dataset_integrity
+nnUNetv2_plan_and_preprocess -d 301 -c 3d_fullres -np 3 --verify_dataset_integrity
 
 # Train
-nnUNetv2_train 304 2d all
+nnUNetv2_train 301 3d_fullres all
