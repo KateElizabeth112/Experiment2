244b8c8dee5d7b83b7d72e4d2d360a502cec2dbf
Kate Cevora <kate.wilkinson@illumr.com>
2023-08-24 10:33:39 +0100

    changed workdir

--- downloadData.sh
+++ downloadData.sh
@@ -3,4 +3,9 @@
 #PBS -l select=1:ncpus=4:mem=8gb
 #PBS -N download_data
 
+cd $PBS_O_WORKDIR
+
+module load tools/prod
+module load SciPy-bundle/2022.05-foss-2022a
+
 curl https://zenodo.org/record/6802614/files/Totalsegmentator_dataset.zip?download=1
