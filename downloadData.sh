7777617a14b90ca2c55032beee8e03e94acf2697
Kate Cevora <kate.wilkinson@illumr.com>
2023-08-24 12:25:45 +0100

    Added output file for curl

--- downloadData.sh
+++ downloadData.sh
@@ -8,4 +8,4 @@ cd $PBS_O_WORKDIR
 module load tools/prod
 module load SciPy-bundle/2022.05-foss-2022a
 
-curl https://zenodo.org/record/6802614/files/Totalsegmentator_dataset.zip?download=1
+curl https://zenodo.org/record/6802614/files/Totalsegmentator_dataset.zip?download=1 --output Totalsegmentator_dataset.zip
