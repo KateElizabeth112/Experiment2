c5aeb1dcf3848973b2a78a51d2888bc59fbc065f
Kate Cevora <kate.wilkinson@illumr.com>
2023-08-24 14:46:27 +0100

    Added exception for debugging

--- processTotalSegmentatorData.py
+++ processTotalSegmentatorData.py
@@ -50,6 +50,7 @@ def main():
 
     # Debug
     no_foreground_counter = 0
+    no_orthonormal_counter = 0
 
     # Find folder sxxx
     for fldr in fldrs:
@@ -57,52 +58,56 @@ def main():
             # open the image to determine the size
             print("Processing case {}".format(fldr))
 
-            img_sitk = sitk.ReadImage(os.path.join(input_folder, fldr, "ct.nii.gz"))
-            img_np = sitk.GetArrayFromImage(img_sitk)
+            try:
+                img_sitk = sitk.ReadImage(os.path.join(input_folder, fldr, "ct.nii.gz"))
+                img_np = sitk.GetArrayFromImage(img_sitk)
 
-            # Create an empty container to store the combined label
-            lab_np = np.zeros(img_np.shape)
+                # Create an empty container to store the combined label
+                lab_np = np.zeros(img_np.shape)
 
-            # Open individual label files and combine into one
-            for i in range(len(segmentation_files)):
-                # seg_nii = nib.load(os.path.join(input_folder, fldr, "segmentations", segmentation_files[i]))
-                seg_sitk = sitk.ReadImage(os.path.join(input_folder, fldr, "segmentations", segmentation_files[i]))
+                # Open individual label files and combine into one
+                for i in range(len(segmentation_files)):
+                    # seg_nii = nib.load(os.path.join(input_folder, fldr, "segmentations", segmentation_files[i]))
+                    seg_sitk = sitk.ReadImage(os.path.join(input_folder, fldr, "segmentations", segmentation_files[i]))
 
-                # seg = seg_nii.get_fdata
-                seg_np = sitk.GetArrayFromImage(seg_sitk)
+                    # seg = seg_nii.get_fdata
+                    seg_np = sitk.GetArrayFromImage(seg_sitk)
 
-                # Add the segmentation to the label file
-                lab_np += (i + 1) * seg_np
+                    # Add the segmentation to the label file
+                    lab_np += (i + 1) * seg_np
 
-            # Check that we have labels for at least one foreground region
-            if np.unique(lab_np).shape[0] == 1:
-                no_foreground_counter += 1
-                print("Background only")
-            else:
-                # Copy and rename ct.nii.gz with name in nnUNet format
-                new_img_name = "case_{}_0000.nii.gz".format(fldr[-4:])
-                new_lab_name = "case_{}.nii.gz".format(fldr[-4:])
+                # Check that we have labels for at least one foreground region
+                if np.unique(lab_np).shape[0] == 1:
+                    no_foreground_counter += 1
+                    print("Background only")
+                else:
+                    # Copy and rename ct.nii.gz with name in nnUNet format
+                    new_img_name = "case_{}_0000.nii.gz".format(fldr[-4:])
+                    new_lab_name = "case_{}.nii.gz".format(fldr[-4:])
 
-                # copy across the image to its new destination
-                src = os.path.join(input_folder, fldr, "ct.nii.gz")
-                dest = os.path.join(output_folder, "imagesTr", new_img_name)
-                shutil.copy(src, dest)
+                    # copy across the image to its new destination
+                    src = os.path.join(input_folder, fldr, "ct.nii.gz")
+                    dest = os.path.join(output_folder, "imagesTr", new_img_name)
+                    shutil.copy(src, dest)
 
-                # Check that we did not have any overlap of labels between different organs
-                if np.max(lab_np) > len(segmentation_files):
-                    print("We have label overlap!")
-                    max = int(len(segmentation_files))
-                    # Fix mis-labelled regions
-                    lab_np[lab_np > max] = max
+                    # Check that we did not have any overlap of labels between different organs
+                    if np.max(lab_np) > len(segmentation_files):
+                        print("We have label overlap!")
+                        max = int(len(segmentation_files))
+                        # Fix mis-labelled regions
+                        lab_np[lab_np > max] = max
 
-                # Save combined label file in labelsTr
-                lab_sitk = sitk.GetImageFromArray(lab_np)
-                lab_sitk.CopyInformation(img_sitk)
+                    # Save combined label file in labelsTr
+                    lab_sitk = sitk.GetImageFromArray(lab_np)
+                    lab_sitk.CopyInformation(img_sitk)
 
-                sitk.WriteImage(lab_sitk, os.path.join(labels_folder, new_lab_name))
+                    sitk.WriteImage(lab_sitk, os.path.join(labels_folder, new_lab_name))
+            except:
+                no_orthonormal_counter += 1
+                continue
 
     print("Number of images with no foreground: {}".format(no_foreground_counter))
-
+    print("Number of images with no orthonormal: {}".format(no_orthonormal_counter))
 
 if __name__ == "__main__":
     main()
