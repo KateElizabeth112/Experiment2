c6855ed6aa0aa7e245883199dff6267a5ee9c736
Kate Cevora <kate.wilkinson@illumr.com>
2023-08-25 10:56:41 +0100

    Changed to set1

--- run_nnUNet.sh
+++ run_nnUNet.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 #PBS -l walltime=48:00:00
 #PBS -l select=1:ncpus=12:mem=64gb:ngpus=1:gpu_type=RTX6000
-#PBS -N nnUNet_TotalSegmentator_set3
+#PBS -N nnUNet_TS_Set1
 
 cd ${PBS_O_WORKDIR}
 
 # Launch virtual environment
-#module load anaconda3/personal
-#source activate nnUNetv2
-
-source /rds/general/user/kc2322/home/code/AMOS_3D/venv/bin/activate
+module load anaconda3/personal
+source activate nnUNetv2
 
 ## Verify install:
 python -c "import torch;print('Cuda is available: ', torch.cuda.is_available())"
 
 ROOT_DIR='/rds/general/user/kc2322/home/data/TotalSegmentator/'
-TASK='Dataset303_Set3'
+TASK='Dataset301_Set1'
 
 export nnUNet_raw=$ROOT_DIR"nnUNet_raw"
 export nnUNet_preprocessed=$ROOT_DIR"nnUNet_preprocessed"
@@ -26,10 +24,10 @@ echo $nnUNet_preprocessed
 echo $nnUNet_results
 
 # Create dataset.json
-python3 generateDatasetJson.py -r $ROOT_DIR -n $TASK -tc 338
+python3 generateDatasetJson.py -r $ROOT_DIR -n $TASK -tc 296
 
 # Plan and preprocess data
-nnUNetv2_plan_and_preprocess -d 303 -c 3d_fullres -np 3 --verify_dataset_integrity
+nnUNetv2_plan_and_preprocess -d 301 -c 3d_fullres -np 3 --verify_dataset_integrity
 
 # Train
-nnUNetv2_train 303 3d_fullres all
+nnUNetv2_train 301 3d_fullres all
