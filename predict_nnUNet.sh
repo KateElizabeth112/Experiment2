3f5ea67ccdacc34d25699e51addd204c45762e15
Kate Cevora <kate.wilkinson@illumr.com>
2023-09-07 12:46:34 +0200

    Updated to dataset 303

--- predict_nnUNet.sh
+++ predict_nnUNet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l walltime=5:00:00
 #PBS -l select=1:ncpus=12:mem=120gb:ngpus=1:gpu_type=RTX6000
-#PBS -N nnUNet_AMOS_predict
+#PBS -N nnUNet_predict_303
 
 cd ${PBS_O_WORKDIR}
 
@@ -14,7 +14,7 @@ python -c "import torch;print(torch.cuda.is_available())"
 
 # Set environment variables
 ROOT_DIR='/rds/general/user/kc2322/home/data/TotalSegmentator/'
-DATASET="Dataset302_Set2"
+DATASET="Dataset303_Set3"
 
 export nnUNet_raw=$ROOT_DIR"nnUNet_raw"
 export nnUNet_preprocessed=$ROOT_DIR"nnUNet_preprocessed"
@@ -24,7 +24,7 @@ export nnUNet_results=$ROOT_DIR"nnUNet_results"
 INPUT_FOLDER=$ROOT_DIR"nnUNet_raw/"$DATASET"/imagesTs"
 OUTPUT_FOLDER=$ROOT_DIR"inference/"$DATASET"/all"
 
-nnUNetv2_predict -i $INPUT_FOLDER -o $OUTPUT_FOLDER -d 302 -c 3d_fullres -f all
+nnUNetv2_predict -i $INPUT_FOLDER -o $OUTPUT_FOLDER -d 303 -c 3d_fullres -f all
 
 # Run python script to evaluate results
 python3 processResults.py -d $DATASET
