9d8103b3fec9006178acc0f614a21282a213c054
Kate Cevora <kate.wilkinson@illumr.com>
2023-09-21 18:06:04 +0200

    Fixed dataset name

--- run_nnUNet.sh
+++ run_nnUNet.sh
@@ -13,7 +13,7 @@ source activate nnUNetv2
 python -c "import torch;print('Cuda is available: ', torch.cuda.is_available())"
 
 ROOT_DIR='/rds/general/user/kc2322/home/data/TotalSegmentator/'
-TASK='Dataset501_fold0'
+TASK='Dataset501_Fold0'
 
 export nnUNet_raw=$ROOT_DIR"nnUNet_raw"
 export nnUNet_preprocessed=$ROOT_DIR"nnUNet_preprocessed"
@@ -27,7 +27,7 @@ echo $nnUNet_results
 python3 generateDatasetJson.py -r $ROOT_DIR -n $TASK -tc 304
 
 # Plan and preprocess data
-#nnUNetv2_plan_and_preprocess -d 501 -c 3d_fullres -np 3 --verify_dataset_integrity
+nnUNetv2_plan_and_preprocess -d 501 -c 3d_fullres -np 3 --verify_dataset_integrity
 
 # Train
-#nnUNetv2_train 501 3d_fullres all
+nnUNetv2_train 501 3d_fullres all
