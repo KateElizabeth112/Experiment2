507878e5e3efbf486a187e8add01bd85a8b7c643
Kate Cevora <kate.wilkinson@illumr.com>
2023-09-28 10:23:34 +0100

    Updated to 800

--- predict_nnUNet.sh
+++ predict_nnUNet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l walltime=10:00:00
 #PBS -l select=1:ncpus=12:mem=120gb:ngpus=1:gpu_type=RTX6000
-#PBS -N nnUNet_predict_700
+#PBS -N nnUNet_predict_800
 
 cd ${PBS_O_WORKDIR}
 
@@ -15,8 +15,8 @@ python -c "import torch;print(torch.cuda.is_available())"
 # Set environment variables
 ROOT_DIR='/rds/general/user/kc2322/home/data/TotalSegmentator/'
 
-datasets=("Dataset701_Fold2", "Dataset702_Fold2", "Dataset703_Fold2")
-tasks=(701, 702, 703)
+datasets=("Dataset801_Fold3", "Dataset802_Fold3", "Dataset803_Fold3")
+tasks=(801, 802, 803)
 
 export nnUNet_raw=$ROOT_DIR"nnUNet_raw"
 export nnUNet_preprocessed=$ROOT_DIR"nnUNet_preprocessed"
