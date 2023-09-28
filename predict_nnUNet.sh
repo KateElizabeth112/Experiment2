377bfb4cbb02013a73220b5dd9c0e0c4f73a9e57
Kate Cevora <kate.wilkinson@illumr.com>
2023-09-28 10:22:35 +0100

    Updated to 700

--- predict_nnUNet.sh
+++ predict_nnUNet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l walltime=10:00:00
 #PBS -l select=1:ncpus=12:mem=120gb:ngpus=1:gpu_type=RTX6000
-#PBS -N nnUNet_predict_601
+#PBS -N nnUNet_predict_700
 
 cd ${PBS_O_WORKDIR}
 
@@ -15,14 +15,14 @@ python -c "import torch;print(torch.cuda.is_available())"
 # Set environment variables
 ROOT_DIR='/rds/general/user/kc2322/home/data/TotalSegmentator/'
 
-datasets=("Dataset601_Fold1")
-tasks=(601)
+datasets=("Dataset701_Fold2", "Dataset702_Fold2", "Dataset703_Fold2")
+tasks=(701, 702, 703)
 
 export nnUNet_raw=$ROOT_DIR"nnUNet_raw"
 export nnUNet_preprocessed=$ROOT_DIR"nnUNet_preprocessed"
 export nnUNet_results=$ROOT_DIR"nnUNet_results"
 
-for number in {0..0}; do
+for number in {0..2}; do
     DATASET=${datasets[number]}
     TASK=${tasks[number]}
 
@@ -34,7 +34,6 @@ for number in {0..0}; do
     echo $DATASET
     echo $INPUT_FOLDER
     echo $OUTPUT_FOLDER
-
 
     nnUNetv2_predict -i $INPUT_FOLDER -o $OUTPUT_FOLDER -d $TASK -c 3d_fullres -f all
 
